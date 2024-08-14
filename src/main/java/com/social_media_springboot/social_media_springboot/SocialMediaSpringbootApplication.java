330e498e67ae8da2610a5a9f02b3de34a17ab396
Joshua579 <falkjoshua2200@gmail.com>
2024-08-14 08:56:21 +0200

    Initial commit

---
+++ src/main/java/com/social_media_springboot/social_media_springboot/SocialMediaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.social_media_springboot.social_media_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social_media_springboot/social_media_springboot/SocialMediaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social_media_springboot.social_media_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
