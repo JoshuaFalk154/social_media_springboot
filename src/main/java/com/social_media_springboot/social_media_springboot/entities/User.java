e9cbe90c9adb0ef345c11561d4f5b8cfe72e98cd
Joshua579 <falkjoshua2200@gmail.com>
2024-08-15 11:32:59 +0200

    UserDetails and Repo-Method

--- src/main/java/com/social_media_springboot/social_media_springboot/entities/User.java
+++ src/main/java/com/social_media_springboot/social_media_springboot/entities/User.java
@@ -1,16 +1,23 @@
 package com.social_media_springboot.social_media_springboot.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name="users")
 
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -41,5 +48,38 @@ public class User {
     private Date updatedAt;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
--- src/main/java/com/social_media_springboot/social_media_springboot/repositories/UserRepository.java
+++ src/main/java/com/social_media_springboot/social_media_springboot/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.social_media_springboot.social_media_springboot.repositories;
 import com.social_media_springboot.social_media_springboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository  extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
