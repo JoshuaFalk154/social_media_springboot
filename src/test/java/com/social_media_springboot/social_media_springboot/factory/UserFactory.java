46bb1ff14d9c8425da1f256e0fef404e64c641b3
Joshua579 <falkjoshua2200@gmail.com>
2024-08-23 13:06:55 +0200

    UserRepoTest and UserFactory implemented

---
+++ src/test/java/com/social_media_springboot/social_media_springboot/factory/UserFactory.java
@@ -0,0 +1,23 @@
+package com.social_media_springboot.social_media_springboot.factory;
+
+import com.social_media_springboot.social_media_springboot.entities.User;
+
+public class UserFactory {
+    private static int userCounter = 1;
+
+    public static User createValidUser() {
+        return createValidUser("validUser" + userCounter++, "valid" + userCounter + "@gmail.com", "password" + userCounter);
+    }
+
+    public static User createValidUser(String username, String email, String password) {
+        return createUser(username, email, password);
+    }
+
+    public static User createUser(String username, String email, String password) {
+        return User.builder()
+                .username(username)
+                .email(email)
+                .password(password)
+                .build();
+    }
+}
--- src/test/java/com/social_media_springboot/social_media_springboot/repositories/UserRepositoryTest.java
+++ src/test/java/com/social_media_springboot/social_media_springboot/repositories/UserRepositoryTest.java
@@ -1,15 +1,20 @@
 package com.social_media_springboot.social_media_springboot.repositories;
 
-import com.social_media_springboot.social_media_springboot.entities.Post;
 import com.social_media_springboot.social_media_springboot.entities.User;
-import jakarta.persistence.EntityManager;
+import com.social_media_springboot.social_media_springboot.factory.UserFactory;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -18,39 +23,67 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PostRepository postRepository;
-
-    @Autowired
-    private EntityManager entityManager;
-
-
-    @Test
-    public void UserRepository_Save_ReturnSavedUser() {
-        User user = User.builder()
-                .username("falk")
-                .email("falk@gmail.com")
-                .password("password")
-                .build();
-        User currentUser = userRepository.save(user);
-
-        Post post = Post.builder()
-                .title("Test Post")
-                .content("This is a test post")
-                .owner(currentUser)
-                .isPublic(true)
-                .build();
-        postRepository.save(post);
-
-
-        entityManager.flush();
-        entityManager.refresh(currentUser);
-
-        Assertions.assertThat(currentUser).isNotNull();
-        Assertions.assertThat(currentUser.getId()).isEqualTo(1);
-        Assertions.assertThat(currentUser.getUsername()).isEqualTo("falk");
-        Assertions.assertThat(currentUser.getPosts()).hasSize(1);
-        Assertions.assertThat(currentUser.getPosts().get(0).getTitle()).isEqualTo("Test Post");
+    @AfterEach
+    public void cleanUp() {
+        userRepository.deleteAll();
     }
 
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void save_ValidUser_ReturnsSavedUser() {
+        User savedUser = userRepository.save(UserFactory.createValidUser());
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isNotNull();
+    }
+
+    @Test
+    public void findAll_ReturnsAllUsers() {
+        User user1 = userRepository.save(UserFactory.createValidUser());
+        User user2 = userRepository.save(UserFactory.createValidUser());
+
+        List<User> foundUsers = userRepository.findAll();
+
+        Assertions.assertThat(foundUsers).isNotNull();
+        Assertions.assertThat(foundUsers.size()).isEqualTo(2);
+        Assertions.assertThat(foundUsers).containsExactlyInAnyOrder(user1, user2);
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void findById_ExistingId_ReturnsUserOptional() {
+        User savedUser = userRepository.save(UserFactory.createValidUser());
+
+        Optional<User> userOptional = userRepository.findById(savedUser.getId());
+
+        Assertions.assertThat(userOptional).isPresent();
+        Assertions.assertThat(userOptional.get()).isEqualTo(savedUser);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "newUsername1",
+            "newUsername2"
+    })
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void updateUser_ValidUser_ReturnsUpdatedUser(String updatedUsername) {
+        User savedUser = userRepository.save(UserFactory.createValidUser());
+
+        savedUser.setUsername(updatedUsername);
+        User updatedUser = userRepository.save(savedUser);
+
+        Assertions.assertThat(updatedUser).isNotNull();
+        Assertions.assertThat(updatedUser.getUsername()).isEqualTo(updatedUsername);
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void deleteById_ExistingId_RemovesUser() {
+        User savedUser = userRepository.save(UserFactory.createValidUser());
+
+        userRepository.deleteById(savedUser.getId());
+        Optional<User> userOptional = userRepository.findById(savedUser.getId());
+
+        Assertions.assertThat(userOptional).isEmpty();
+    }
 }
